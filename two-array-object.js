af4f7528e0553dfbd77c92c6823f0d2c125828de
Kevin Templeton <templeton.kevin@gmail.com>
2023-03-16 23:09:19 -0500

    twoArrayObject completed and tests passing

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,14 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+/**
+ * Convert the keys array into an object using the reduce method.
+ *  In the reduce callback function, set the current value as a key in
+ *  the accumulator object, with the value set to whatever element is
+ *  at the current index in the values array.
+ */
+function twoArrayObject(keys, values) {
+    return keys.reduce((obj, cur, idx) => {
+        obj[cur] = idx < values.length ? values[idx] : null;
+        return obj;
+    }, {});
 }
+
+module.exports = twoArrayObject;
